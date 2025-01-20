c4bae5eedd03616793eea5181c978d66cf12ab88
Padi Amu <padiamu@gmail.com>
2025-01-20 15:46:16 +0000

    - first commit

---
+++ src/main/java/co/terydin/rabbitproducer/SampleRabbitProducerApplication.java
@@ -0,0 +1,13 @@
+package co.terydin.rabbitproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRabbitProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRabbitProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/terydin/rabbitproducer/SampleRabbitProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.terydin.rabbitproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRabbitProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
