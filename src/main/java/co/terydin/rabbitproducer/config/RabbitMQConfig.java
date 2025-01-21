f6eb4457416a0c752cf58f4c3b15da9ebdc6ab67
Padi Amu <padiamu@gmail.com>
2025-01-21 09:02:07 +0000

    Feat: added a message converter

--- src/main/java/co/terydin/rabbitproducer/config/RabbitMQConfig.java
+++ src/main/java/co/terydin/rabbitproducer/config/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package co.terydin.rabbitproducer.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,4 +27,12 @@ public class RabbitMQConfig {
   public Queue userDeleted() {
     return new Queue("userDeleted", false);
   }
+
+  @Bean
+  public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+    return rabbitTemplate;
+  }
+
 }
